4731f6f6dd5a773d865d7378386ec46afe512dd9
solfer <fsolferini@gmail.com>
2019-08-03 19:31:14 +0100

    Adding challenges 1 to 5

---
+++ set1/challenge02.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+
+# https://www.cryptopals.com/sets/1/challenges/2
+# XOR both inputs
+
+'''def alt_xor(a,b):
+    raw_a = bytearray.fromhex(a)
+    raw_b = bytearray.fromhex(b)
+    
+    xored = bytearray([raw_a[i]^raw_b[i] for i in range(len(raw_a))])
+    print type(xored) 
+    return bytearray.tohex(xored)
+'''
+
+def alt_xor(a,b):
+    return hex(int(a,16)^int(b,16))[2:].replace("L","")
+
+def xor(a,b):
+    raw_a = a.decode("hex")
+    raw_b = b.decode("hex")
+
+    return "".join([chr(ord(raw_a[i])^ord(raw_b[i])) for i in range(len(raw_a))]).encode("hex")
+
+
+INPUT1 = "1c0111001f010100061a024b53535009181c"
+INPUT2 = "686974207468652062756c6c277320657965"
+RESULT = "746865206b696420646f6e277420706c6179"
+
+result = alt_xor(INPUT1,INPUT2)
+
+print RESULT
+print result
+
+print "Success" if RESULT == result else "Fail"
+   
---
+++ set1/challenge05.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+
+# https://www.cryptopals.com/sets/1/challenges/5
+# XOR INPUT with "ICE"
+
+'''def alt_xor(a,b):
+    raw_a = bytearray.fromhex(a)
+    raw_b = bytearray.fromhex(b)
+    
+    xored = bytearray([raw_a[i]^raw_b[i] for i in range(len(raw_a))])
+    print type(xored) 
+    return bytearray.tohex(xored)
+'''
+
+def xor_cyclic(a,b):
+
+    return "".join([chr(ord(a[i])^ord(b[i%len(b)])) for i in range(len(a))]).encode("hex")
+
+
+'''
+INPUT1 = "Burning 'em, if you ain't quick and nimble"
+INPUT2 = "I go crazy when I hear a cymbal"
+
+RESULT1 = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
+RESULT2 = "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+'''
+
+INPUT = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+
+RESULT = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+
+result = xor_cyclic(INPUT,"ICE")
+
+print RESULT
+print result
+
+print "Success" if RESULT == result else "Fail"
+   
