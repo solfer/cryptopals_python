89ee6e45851a8771d03a782d98dda59244af85de
solfer <fsolferini@gmail.com>
2019-08-04 14:32:27 +0100

    Modified pkcs7 function

--- set2/challenge09.py
+++ set2/challenge09.py
@@ -3,21 +3,25 @@
 # https://www.cryptopals.com/sets/2/challenges/9
 # Implement PKCS#7 padding
 
-def pkcs7(data, block_len):
-    pad = block_len - len(data)
+
+def pkcs7_add(data, block_len):
+    pad = block_len - len(data)%block_len
     if pad < 0:
         return None
     elif pad == 0:
         return data + chr(block_len)*block_len
     return data + chr(pad)*pad
 
+def pkcs7_remove(data):
+    pad = ord(data[-1])
+    return data[:-pad]
 
 def main():
 
     INPUT = "YELLOW SUBMARINE"
     RESULT = "YELLOW SUBMARINE\x04\x04\x04\x04"
 
-    result = pkcs7(INPUT,20)
+    result = pkcs7_add(INPUT,20)
 
     print "Success" if RESULT == result else "Fail"
 
